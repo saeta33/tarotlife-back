0405ff7de9fe37eebb66784a81f0de4e90bff4ac
saeta33 <76995396+saeta33@users.noreply.github.com>
2021-01-18 13:46:04 +0900

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ class SystemConfig:
   SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8mb4'.format(**{
       'user': 'admin',
       'password': 'tarot123',
-      'host': 'database-tarot.ccr7qhhszj2y.ap-northeast-1.rds.amazonaws.com',
+      'host': '',
       'db_name': 'tarot'
   })
 
