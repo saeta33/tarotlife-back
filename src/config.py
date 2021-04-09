1df5059599d5dc8cf5a71622e81af8b92d254865
ST <uranianvirgo@yahoo.co.jp>
2021-04-10 02:04:45 +0900

    mod db con

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ class SystemConfig:
   SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8mb4'.format(**{
       'user': os.environ["dbuser"],
       'password': os.environ["dbpass"],
-      #'host': 'db',
+      'host': 'db',
       'host': os.environ["dbhost"],
       'db_name': os.environ["dbuser"]
   })
